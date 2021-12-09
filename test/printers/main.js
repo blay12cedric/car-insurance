412fce55322ef183268988141ad2a7c9b655329d
Cedric Achi <cedric.a@izinov-group.com>
2021-12-09 15:27:21 +0000

    module (printer): fix describe message for Test's suite

--- test/printers/main.js
+++ test/printers/main.js
@@ -2,7 +2,7 @@ import { assert } from "chai";
 import { before, describe, it } from "mocha";
 import { PrinterService } from "../../src/services/index.js";
 
-describe("Test's suite for module Product (model & service)", () => {
+describe("Test's suite for module Printer (service)", () => {
   let input, printerService;
   before("Create the input and a PrinterService()", () => {
     input = "Hello Car Insurance";
